c4a769d73fbcc2085f97c0089f82f1d8eed8641a
Zachary <tsai880806@gmail.com>
2022-05-24 16:00:35 +0800

    Test: Add TC10

--- omniNotes/src/androidTest/java/it/feio/android/omninotes/ui/TC09DiscardNoteChangesTest.java
+++ omniNotes/src/androidTest/java/it/feio/android/omninotes/ui/TC09DiscardNoteChangesTest.java
@@ -4,13 +4,9 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
-
-import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.not;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -19,7 +15,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import it.feio.android.omninotes.R;
-import it.feio.android.omninotes.ui.BaseEspressoTest;
 
 @RunWith(AndroidJUnit4.class)
 public class TC09DiscardNoteChangesTest extends BaseEspressoTest{
@@ -37,5 +32,4 @@ public class TC09DiscardNoteChangesTest extends BaseEspressoTest{
         onView(withId(R.id.detail_content)).check(matches(withText("TestContent")));
         onView(withId(R.id.detail_content)).check(matches(not(withText("TestContentModifiedTest"))));
     }
-
 }
---
+++ omniNotes/src/androidTest/java/it/feio/android/omninotes/ui/TC10ArchiveNotesOnOverviewPage.java
@@ -0,0 +1,56 @@
+package it.feio.android.omninotes.ui;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.longClick;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withParentIndex;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import it.feio.android.omninotes.R;
+
+@RunWith(AndroidJUnit4.class)
+public class TC10ArchiveNotesOnOverviewPage extends BaseEspressoTest{
+
+    @Test
+    public void archiveNotesOnOverviewPage() {
+        createNoteByUI("TestTitle", "TestContent");
+
+        onView(withText("TestTitle")).perform(longClick());
+
+        onView(allOf(
+                withContentDescription("More options"),
+                allOf(
+                        withParent(allOf(
+                                withParent(withId(R.id.action_mode_bar)),
+                                withParentIndex(2))
+                        ), withParentIndex(3)),
+                isDisplayed())
+        ).perform(click());
+        onView(withText("Archive")).perform(click());
+
+//        onView(withText(R.string.note_archived)).perform(click());
+
+        onView(allOf(
+                withContentDescription("drawer open"),
+                allOf(
+                        withParent(withId(R.id.toolbar)),
+                        withParentIndex(0)
+                ),
+                isDisplayed())
+        ).perform(click());
+
+        onView(withText("Archive")).perform(click());
+
+        onView(withText("TestTitle")).check(matches(withText("TestTitle")));
+    }
+}
