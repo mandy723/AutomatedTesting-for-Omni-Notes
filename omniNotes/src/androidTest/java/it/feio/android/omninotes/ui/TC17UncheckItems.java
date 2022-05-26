3af477942ebae2eb6d5d6ab17767abb797eade85
Zachary <tsai880806@gmail.com>
2022-05-27 00:58:49 +0800

    Test: add TC-16 and TC-17

--- omniNotes/src/androidTest/java/it/feio/android/omninotes/ui/TC17UncheckItems.java
+++ omniNotes/src/androidTest/java/it/feio/android/omninotes/ui/TC17UncheckItems.java
@@ -2,23 +2,22 @@ package it.feio.android.omninotes.ui;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withChild;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withParentIndex;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.not;
 
 import org.junit.Test;
 
-public class TC15DisableChecklist extends BaseEspressoTest{
-
+public class TC17UncheckItems extends BaseEspressoTest{
     @Test
-    public void disableChecklist() {
+    public void checkItemsInsideEditingPage() {
         createNoteByUI("TestTitle", "TestContent");
         onView(withText("TestTitle")).perform(click());
         onView(withContentDescription("More options")).perform(click());
@@ -27,17 +26,17 @@ public class TC15DisableChecklist extends BaseEspressoTest{
                 withParent(
                         withChild(withText("TestContent"))),
                 withParentIndex(1))
-        ).check(matches(isDisplayed()));
-
+        ).check(matches(isDisplayed()))
+        .perform(click());
         navigateUp();
-        onView(withText("TestTitle")).perform(click());
-        onView(withContentDescription("More options")).perform(click());
-        onView(withText("Disable checklist")).perform(click());
 
+        onView(withText("TestTitle")).perform(click());
         onView(allOf(
                 withParent(
                         withChild(withText("TestContent"))),
                 withParentIndex(1))
-        ).check(doesNotExist());
+        ).check(matches(isDisplayed()))
+                .perform(click())
+                .check(matches(not(isChecked())));
     }
 }
